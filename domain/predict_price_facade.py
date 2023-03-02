a0b23dd3696af930c9b17b9350670f4b8c63ff9f
albint3r <albint3r@gmail.com>
2023-03-02 00:04:59 -0600

    Add documentation

--- domain/predict_price_facade.py
+++ domain/predict_price_facade.py
@@ -6,17 +6,32 @@ from infrastructure.price_predictor.i_predict_price_facade import IPredictPriceF
 
 
 class PredictPriceFacade(IPredictPriceFacade):
+    """ Facade that predicts the price of a given listing model using a trained model."""
 
     def __init__(self, trained_model: ITrainedModels, listing_model: IListingsModel):
+        """ Initializes a new instance of the PredictPriceFacade class.
+
+        Parameters:
+            trained_model (ITrainedModels): The trained model to use for prediction.
+            listing_model (IListingsModel): The listing model to predict the price for.
+        """
         self.price: np.array = None
         self.trained_model: ITrainedModels = trained_model
         self.listing: IListingsModel = listing_model
 
     def predict(self) -> dict[str, float]:
+        """ Predicts the price of the listing model using the trained model.
+
+        Returns:
+            dict[str, float]: A dictionary containing the predicted price."""
         listing_array = self.listing.to_array()
         self.trained_model.load()
         self.price = self.trained_model.predict(listing_array)
         return self.price_to_json()
 
     def price_to_json(self):
+        """ Converts the predicted price to a JSON format.
+
+        Returns:
+            dict[str, float]: A dictionary containing the predicted price."""
         return {'data': self.price[0]}
--- main.py
+++ main.py
@@ -10,6 +10,21 @@ app = FastAPI()
 
 @app.get('/house/')
 async def index(m2_land: float, m2_const: float, rooms: int, baths: int, cars: int, lat: float, long: float):
+    """
+    Endpoint that predicts the price of a house based on its features.
+
+    Parameters:
+        m2_land (float): Total square meters of the land.
+        m2_const (float): Total square meters of the house.
+        rooms (int): Number of rooms.
+        baths (int): Number of bathrooms.
+        cars (int): Number of parking spots.
+        lat (float): Latitude of the house.
+        long (float): Longitude of the house.
+
+    Returns:
+        float: The predicted price of the house.
+    """
     house = House(m2_land=m2_land, m2_const=m2_const, rooms=rooms, baths=baths, cars=cars, lat=lat, long=long)
     house_price_predictor = HousePricePredictor()
     facade = PredictPriceFacade(listing_model=house, trained_model=house_price_predictor)
@@ -19,6 +34,20 @@ async def index(m2_land: float, m2_const: float, rooms: int, baths: int, cars: i
 
 @app.get('/apartments/')
 async def index(m2_const: float, rooms: int, baths: int, cars: int, lat: float, long: float):
+    """
+    Endpoint that predicts the price of an apartment based on its features.
+
+    Parameters:
+        m2_const (float): Total square meters of the apartment.
+        rooms (int): Number of rooms.
+        baths (int): Number of bathrooms.
+        cars (int): Number of parking spots.
+        lat (float): Latitude of the apartment.
+        long (float): Longitude of the apartment.
+
+    Returns:
+        float: The predicted price of the apartment.
+    """
     apartment = Apartment(m2_const=m2_const, rooms=rooms, baths=baths, cars=cars, lat=lat, long=long)
     apartment_price_predictor = ApartmentPricePredictor()
     facade = PredictPriceFacade(listing_model=apartment, trained_model=apartment_price_predictor)
